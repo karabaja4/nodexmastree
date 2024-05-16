ff0ad20b866a2df9c0573c03d45e555a11f59d19
Igor Saric <igor.saric@outlook.com>
2024-05-16 21:32:05 +0200

    fix brightness

--- tree.js
+++ tree.js
@@ -18,6 +18,7 @@ process.on('SIGTERM', shutdown);
 let brightness = 0.5;
 const setBrightness = (value) => {
   brightness = Math.max(0, Math.min(1, value));
+  console.log(`Set brightness to ${brightness}`);
 };
 
 const getBrightnessByte = () => {
@@ -42,9 +43,8 @@ const sendByte = (value) => {
   }
 };
 
-const brightnessByte = getBrightnessByte();
 const sendLed = (r, g, b) => {
-  sendByte(brightnessByte);
+  sendByte(getBrightnessByte());
   sendByte(b);
   sendByte(g);
   sendByte(r);

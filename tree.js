6568e4f7cd19e10a09c0ec327b3329d1049c5e23
Igor Saric <igor.saric@outlook.com>
2024-05-16 21:37:33 +0200

    shutdown fix

--- tree.js
+++ tree.js
@@ -52,8 +52,7 @@ const sendLed = (r, g, b) => {
 
 const update = async (pixels) => {
   if (!pixels || pixels.length !== pixelCount) {
-    console.log('Invalid pixel count.');
-    process.exit(1);
+    shutdown();
   }
   for (let i = 0; i < 4; i++) {
     sendByte(0);

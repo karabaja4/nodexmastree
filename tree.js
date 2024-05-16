809f14a349426d795c9bdab12962d49d333fe564
Igor Saric <igor.saric@outlook.com>
2024-05-16 21:38:18 +0200

    logging

--- tree.js
+++ tree.js
@@ -52,6 +52,7 @@ const sendLed = (r, g, b) => {
 
 const update = async (pixels) => {
   if (!pixels || pixels.length !== pixelCount) {
+    console.log('Invalid pixel count.');
     shutdown();
   }
   for (let i = 0; i < 4; i++) {
